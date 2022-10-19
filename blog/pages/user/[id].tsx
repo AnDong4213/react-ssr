5d785b8e4c02bb86366975922721e186f799e9ef
赵 安东 <zhaoandong@bobfintech.com.cn>
2022-10-19 18:14:32 +0800

    fix: tag

--- blog/pages/user/[id].tsx
+++ blog/pages/user/[id].tsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
 import { observer } from 'mobx-react-lite';
 import { Button, Avatar, Divider } from 'antd';
+import {
+  CodeOutlined,
+  FireOutlined,
+  FundViewOutlined,
+} from '@ant-design/icons';
+import ListItem from 'components/ListItem';
+import { prepareConnection } from 'db/index';
+import { User, Article } from 'db/entity';
 import styles from './index.module.scss';
 
 const UserDetail = () => {
